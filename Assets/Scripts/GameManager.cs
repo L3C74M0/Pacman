256fb60952d3cb3d711f4e6dde116b59761b786a
DEGED <47962123+DEGED@users.noreply.github.com>
2022-04-06 15:14:42 +0200

    Ghost prefabs Done 2:00:31

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : MonoBehaviour {
     public Ghost[] ghosts; 
     public Pacman pacman;
     public Transform pellets;
+
+    public int ghostMultiplayer { get; private set; } = 1;
     
     public int score { get; private set; }
 
@@ -27,16 +29,19 @@ public class GameManager : MonoBehaviour {
     }
 
     private async void NewRound(){
-        foreach (Transform pellet in pellets) {
+        foreach (Transform pellet in this.pellets) {
             pellet.gameObject.SetActive(true);
         }
 
         ResetState();
     }
-
+         
     private void ResetState(){
-        for( int i = 0 ; i < ghosts.Length; i++) {
-            ghosts[i].gameObject.SetActive(true);
+
+        ResetGhostMultiplayer();
+
+        for( int i = 0 ; i < this.ghosts.Length; i++) {
+            this.ghosts[i].gameObject.SetActive(true);
         }
 
         pacman.gameObject.SetActive(true);
@@ -59,19 +64,60 @@ public class GameManager : MonoBehaviour {
         this.lives = lives;
     }
 
-    public void GhostEaten (Ghost ghost) {
+    public void GhostEaten(Ghost ghost) {
+        int points = ghost.points * this.ghostMultiplayer;
+        SetScore(this.score + points);
         SetScore(this.score + ghost.points);
     }
 
     public void PacmanEaten () {
         this.pacman.gameObject.SetActive(false);
 
-        SetLives(this.lives--);
+        SetLives(this.lives-1);
 
         if(this.lives > 0){
             Invoke(nameof(ResetState), 3.0f);
         } else {
             GameOver();
         }
+
+    }
+
+    public void PelletEaten(Pellet pellet)    {
+        pellet.gameObject.SetActive(false);
+
+        SetScore(this.score + pellet.points);
+
+        if (!HasRemainingPellets())
+        {
+            this.pacman.gameObject.SetActive(false);
+            Invoke(nameof(NewRound), 3.0f);
+        }
+    }
+    public void PowerPelletEaten (PowerPellet pellet)
+    {
+
+
+        // TODO: changing ghost state        
+        PelletEaten(pellet);
+        CancelInvoke();
+        Invoke(nameof(ResetGhostMultiplayer), pellet.duration);
+    }
+
+    private bool HasRemainingPellets()    {
+        foreach (Transform pellet in this.pellets)
+        {
+            if (pellet.gameObject.activeSelf)
+            {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+
+    private void ResetGhostMultiplayer()
+    {
+        this.ghostMultiplayer = 1;
     }
 }
