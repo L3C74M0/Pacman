bbf3e4f79636d82a656574f8c3e62e74ce700c7d
DEGED <47962123+DEGED@users.noreply.github.com>
2022-04-11 16:00:51 +0200

    Game Done 3:24:00

---
+++ Assets/Scripts/GhostEyes.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class GhostEyes : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Movement movement { get; private set; }
+    public Sprite up;
+    public Sprite down;
+    public Sprite left;
+    public Sprite right;
+
+    private void Awake()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.movement = GetComponentInParent<Movement>();
+    }
+
+    private void Update()
+    {
+        if(this.movement.direction == Vector2.up)
+        {
+            this.spriteRenderer.sprite = this.up;
+        }
+        if (this.movement.direction == Vector2.down)
+        {
+            this.spriteRenderer.sprite = this.down;
+        }
+        if (this.movement.direction == Vector2.left)
+        {
+            this.spriteRenderer.sprite = this.left;
+        }
+        if (this.movement.direction == Vector2.right)
+        {
+            this.spriteRenderer.sprite = this.right;
+        }
+    }
+}
