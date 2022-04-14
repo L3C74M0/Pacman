7bd48072f5fb96fe82a2c9d8954e29f0b274a5af
L3C74M0 <47828446+L3C74M0@users.noreply.github.com>
2022-04-14 20:25:55 +0200

    Added game start scene and credits

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+     
+    public void OpenScene(int index) {
+        SceneManager.LoadScene(index);
+    }
+}
